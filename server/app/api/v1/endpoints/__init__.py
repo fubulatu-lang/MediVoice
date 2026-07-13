83723c91f9d0cba10b6e29612355abf8f12f4736
fubulatu-lang <fubulatu@gmail.com>
2026-07-13 19:52:01 +0000

    Update __init__.py

--- server/app/api/v1/endpoints/__init__.py
+++ server/app/api/v1/endpoints/__init__.py
@@ -1,3 +1,5 @@
-"""
-API Endpoints Package
-"""
+""" API Endpoints Package """
+from .auth import router as auth_router
+from .transcription import router as transcription_router
+from .formatting import router as formatting_router
+from .health import router as health_router
