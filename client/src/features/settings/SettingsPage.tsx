cb544a388af2e95e66f7b09905e2620dbd0f6c79
fubulatu-lang <fubulatu@gmail.com>
2026-07-17 01:41:33 +0000

    Update SettingsPage.tsx

--- client/src/features/settings/SettingsPage.tsx
+++ client/src/features/settings/SettingsPage.tsx
@@ -2,7 +2,7 @@ const SettingsPage = () => {
   return (
     <div className="settings">
       <h2>Settings</h2>
-      <p>Template preferences, language, etc. (Coming soon)</p>
+      <p>Template preferences, language, and app settings coming soon.</p>
     </div>
   );
 };
