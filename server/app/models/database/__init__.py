7781ca190d9d88de10277cf762277c8b81a9b3b7
fubulatu-lang <fubulatu@gmail.com>
2026-07-18 03:14:59 +0000

    Update __init__.py

--- server/app/models/database/__init__.py
+++ server/app/models/database/__init__.py
@@ -1,3 +1,7 @@
-"""
-Database Models Package
-"""
+# server/app/models/database/__init__.py
+
+from .base import Base
+from .note import Note
+
+# This ensures all models are imported and registered with SQLAlchemy.
+# Add other model imports here as they are created.
