0cbee299f387edd5ddc9812dc8a425acefaf15c3
fubulatu-lang <fubulatu@gmail.com>
2026-07-18 08:19:46 +0000

    Delete __init__.py

--- server/app/models/database/__init__.py
+++
@@ -1,7 +0,0 @@
-# server/app/models/database/__init__.py
-
-from .base import Base
-from .note import Note
-
-# This ensures all models are imported and registered with SQLAlchemy.
-# Add other model imports here as they are created.
