1c899fe7513e45942b212ba8496c41c30cfc9cb2
fubulatu-lang <fubulatu@gmail.com>
2026-07-18 13:24:10 +0000

    Create RecordingPage.tsx

---
+++ client/src/features/recording/RecordingPage.tsx
@@ -0,0 +1,10 @@
+const RecordingPage = () => {
+  return (
+    <div>
+      <h1>Recording Page</h1>
+      <p>Speech recorder will go here.</p>
+    </div>
+  );
+};
+
+export default RecordingPage;
